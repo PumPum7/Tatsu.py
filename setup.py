23c05307eb5117fbc44a3cbb680dd31762d2ba96
Pum <michael.duengler@gmail.com>
2018-08-14 14:33:44 +0200

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='Tatsumaki.py',
-    version='1',
-    packages=['aiohttp==3.3.2',],
-    license='GNU General Public License v3.0',
-    long_description=open('README.md').read(),
-)
