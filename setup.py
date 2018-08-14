1080e29f7d68f458b5e35ce2876d81abcbdbee95
Pum <michael.duengler@gmail.com>
2018-08-14 11:11:02 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Tatsumaki.py',
+    version='1',
+    packages=['aiohttp==3.3.2',],
+    license='GNU General Public License v3.0',
+    long_description=open('README.md').read(),
+)
